530488ba8993e3e7d9b09af7f639065ebcd220e0
Yoel Dirar <yoeldirar@Yoels-MBP.attlocal.net>
2021-08-04 11:27:44 -0700

    refactor files

---
+++ Dirar.Yoel/lib/php/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+function  MYSQLIAuth(){
+	return [
+
+   "localhost", //mysqul host 
+    "wnm608Dirar_Yoel", //mysql username 
+    "wnm608Dirar_Yoel", // mysql password
+   "wnm608Dirar_Yoel" // mysql databse name 
+	];
+}
